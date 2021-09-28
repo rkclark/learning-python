a2532632d2c4cdbd4cf6a96f960dd045bc8125dc
rkclark <rkingsleyclark@gmail.com>
2021-09-28 15:44:54 +0100

    more

---
+++ builtInFunctions.py
@@ -0,0 +1,42 @@
+# absolute value
+
+absolute = abs(-5) # 5
+print(absolute)
+
+# min / max from list
+
+list = [2,3,5,6,4,3,4,10]
+
+minValue = min(list)
+maxValue = max(list)
+
+print(minValue, maxValue)
+
+# rounding
+
+x = 5.222
+
+rounded = round(x)
+
+print(rounded)
+
+# parse integer
+
+intAsString = '55'
+integer = int(intAsString)
+
+print(integer)
+
+# parse float
+
+floatString = '55.12'
+parsedFloat = float(floatString)
+
+print(parsedFloat)
+
+# parse string
+
+notString = 77
+string = str(notString)
+
+print(string)
