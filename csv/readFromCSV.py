806ab42c1790ed0e820aa612d2e7b886b9e44f46
rkclark <rkingsleyclark@gmail.com>
2021-09-28 09:00:29 +0100

    More

---
+++ csv/readFromCSV.py
@@ -0,0 +1,23 @@
+import csv
+
+with open('./csv/example.csv') as csvfile:
+  csvContent = csv.reader(csvfile, delimiter=',')
+
+  dates = []
+  colours = []
+  for row in csvContent:
+    colour = row[3]
+    date = row[0]
+
+    dates.append(date)
+    colours.append(colour)
+    print(row)
+
+  print(dates, colours)
+
+  whatColour = input('What colour do you wish to know the date of?')
+  index = colours.index(whatColour)
+
+  desiredDate = dates[index]
+
+  print('The date of ', whatColour, ' is ', desiredDate)
