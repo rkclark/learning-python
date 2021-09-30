e5a6f26df54f1b86031d2d0a4d48d2e0a0eba57a
rkclark <rkingsleyclark@gmail.com>
2021-09-30 11:35:53 +0100

    more

--- builtInModules/urllibModule.py
+++ builtInModules/urllibModule.py
@@ -12,12 +12,15 @@ print(x.read())
 
 url = 'http://pythonprogramming.net'
 values = {'s': 'basic', 'submit': 'search'}
+headers = {
+  'my': 'header'
+}
 
 # encode to create query string from dictionary
 data = urllib.parse.urlencode(values)
 # encode to utf-8
 data = data.encode('utf-8')
-request = urllib.request.Request(url, data)
+request = urllib.request.Request(url, data, headers=headers)
 response = urllib.request.urlopen(request)
 responseData = response.read()
 
---
+++ regexes.py
@@ -0,0 +1,14 @@
+import re
+
+exampleString='''
+Jessica is 15 years old, Daniel is 27 years old.
+Edward is 97, his grandfather, Oscar, is 102.
+'''
+
+# findall takes regex as first arg, target string as second
+ages = re.findall(r'\d{1,3}', exampleString)
+
+names = re.findall(r'[A-Z][a-z]*', exampleString)
+
+print(ages)
+print(names)
