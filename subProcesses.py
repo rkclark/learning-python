36df887caf3eb81a7c52e3132fdc74111f740378
rkclark <rkingsleyclark@gmail.com>
2021-10-04 11:44:51 +0100

    more

---
+++ subProcesses.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# Call a shell command from inside python
+subprocess.call("ls", shell=True)
+
+print("*****\n")
+
+# Run the same command but this time save the output into a variable
+output = subprocess.check_output("ls", shell=True)
+print(output)
